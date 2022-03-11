4160693a5e7e712bb93b47c736ec5b27c043ea3e
Gajam_Jyothsna <jyothsna.gajam@yesgnome.com>
2022-03-11 07:56:41 +0530

    Randomly generated the main background color

---
+++ Assets/CameraBackground.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBackground : MonoBehaviour
+{
+    public Color[] color = new Color[5];
+    float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = time + Time.deltaTime; 
+        if (time > 5f)
+        {
+            Camera.main.backgroundColor = color[Random.Range(0, 5)];
+            time = 0f;
+        }
+    }
+}
